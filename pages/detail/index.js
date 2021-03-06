2839e3afba656da755e8eb4cfb9e028d2739397d
老太头 <m13949411607@163.com>
2021-03-06 15:15:09 +0800

    fix: 当没有readme的项目进行兼容，解决报错500的问题

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -3,14 +3,18 @@ import api from '../../lib/api'
 import 'github-markdown-css'
 import MdRenderer from '../../components/MarkdownRender'
 
-const Detail = ({readme}) => {
-  return <MdRenderer content={readme.content} isBase64={true}/>
+const Detail = ({ readme }) => {
+  return readme ? <MdRenderer content={readme.content} isBase64={true} /> : <div>这个项目没有readme</div>
 }
-Detail.getInitialProps = async ({ ctx: { query: {owner, name}, req, res } }) => {
-
-  const readmeResp = await api.request({
-    url: `/repos/${owner}/${name}/readme`
-  }, req, res)
+Detail.getInitialProps = async ({ ctx: { query: { owner, name }, req, res } }) => {
+  let readmeResp
+  try {
+    readmeResp = await api.request({
+      url: `/repos/${owner}/${name}/readme`
+    }, req, res)
+  } catch (e) {
+    readmeResp = {}
+  }
   return {
     readme: readmeResp.data
   }
